74c046f4411b44b940291ec0fe0eb5c7212cc2c5
Ivan Salas <nochisalas2002@gmail.com>
2025-07-02 18:09:18 -0600

    Primer lanzamiento del paquete

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MiQuintoRepo",                                     # Nombre del paquete
+    version="0.1.0",                                         # Versión inicial
+    packages=find_packages(),                                # Paquetes a incluir
+    description="Un paquete pip simple de saludo",           # Breve descripción
+    author="Ivan Salas",                                     # Tu nombre
+    author_email="nochisalas2002@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/ivansalas2064/MiQuintoRepo",     # URL del proyecto
+)
